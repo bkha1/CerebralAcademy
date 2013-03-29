ef5484b9e3e097576f1643d349552acf05e41568
Brian Kha <brian.kha@asu.edu>
2013-03-28 17:31:27 -0700

    fixed doortrigger so that E works now

--- Assets/Levels/Scripts/DoorTrigger.cs
+++ Assets/Levels/Scripts/DoorTrigger.cs
@@ -25,15 +25,25 @@ public class DoorTrigger : MonoBehaviour {
             param.Add("duration", 1.0f);
             NotificationCenter.DefaultCenter.PostNotification(this, "DisplayText", param);
 			
-		if(Input.GetKeyDown(KeyCode.E))
-		{
-			Hashtable param1 = new Hashtable();
-            param.Add("gameObject", this.gameObject);
-            param.Add("target", new Vector3(0.0f, 0.0f, 0.0f));
-            param.Add("isLevel", true);
-            param.Add("level", levelToLoad);
-            NotificationCenter.DefaultCenter.PostNotification(this, "TeleportPlayerEvent", param1);
-		}
+//			if(Input.GetKeyDown(KeyCode.E))
+//			{
+//				Hashtable param1 = new Hashtable();
+//	            param.Add("gameObject", this.gameObject);
+//	            param.Add("target", new Vector3(0.0f, 0.0f, 0.0f));
+//	            param.Add("isLevel", true);
+//	            param.Add("level", levelToLoad);
+//	            NotificationCenter.DefaultCenter.PostNotification(this, "TeleportPlayerEvent", param1);
+//			}
+			
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				Hashtable param1 = new Hashtable();
+	            param1.Add("gameObject", this.gameObject);
+	            param1.Add("target", new Vector3(0.0f, 0.0f, 0.0f));
+	            param1.Add("isLevel", true);
+	            param1.Add("level", levelToLoad);
+	            NotificationCenter.DefaultCenter.PostNotification(this, "TeleportPlayerEvent", param1);
+			}
 			
 		}
 	}
@@ -42,6 +52,7 @@ public class DoorTrigger : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
+			
             //Create TeleportEvent passing gameObject (player), targetLocation, isLevel, level name
 			/*
             Hashtable param = new Hashtable();
