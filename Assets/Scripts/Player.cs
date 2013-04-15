9553898946959feaaf0fb5ede22746290614d2b9
majora2007 <josephmajora@gmail.com>
2013-04-15 10:32:14 -0700

    Added RelaxationTechnique and CognitivSkill enums to Player to allow setting the current skill or technique.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Player {
 	
+    public enum RelaxationTechnique { };
+    public enum CognitivSkill { LIFT, PUSH, PULL };
 
 	public Player() {
         UserName = string.Empty;
@@ -19,4 +21,7 @@ public class Player {
     public bool hasLearnedLift { get; set; } // If the player has learned Lift and thus can use it
     public bool hasLearnedPush { get; set; } // If the player has learned Push and thus can use it
     public bool hasLearnedPull { get; set; } // If the player has learned Pull and thus can use it
+
+    public CognitivSkill CurrentSkillEquipped { get; set; };
+    public RelaxationTechnique CurrentRelaxationEquipped { get; set; };
 }
